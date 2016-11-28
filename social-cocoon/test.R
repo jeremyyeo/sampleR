4ea6b6d31fbb874fdb7f862064f577efcefbdaf8
Jeremy Yeo <jeremy.yeo@xero.com>
2016-11-28 16:14:09 +1300

    cleanup social-cocoon

--- social-cocoon/test.R
+++ social-cocoon/test.R
@@ -1,14 +1,27 @@
 library(ROAuth)
 library(streamR)
-requestURL <- "https://api.twitter.com/oauth/request_token"
-accessURL <- "https://api.twitter.com/oauth/access_token"
-authURL <- "https://api.twitter.com/oauth/authorize"
-consumerKey <- ""
+
+
+requestURL     <- "https://api.twitter.com/oauth/request_token"
+accessURL      <- "https://api.twitter.com/oauth/access_token"
+authURL        <- "https://api.twitter.com/oauth/authorize"
+consumerKey    <- ""
 consumerSecret <- ""
-my_oauth <- OAuthFactory$new(consumerKey = consumerKey, consumerSecret = consumerSecret,
-                             requestURL = requestURL, accessURL = accessURL, authURL = authURL)
-my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
-save(my_oauth, file = ".httr-oauth")
+
+oauth.file <-
+  OAuthFactory$new(
+    consumerKey = consumerKey,
+    consumerSecret = consumerSecret,
+    requestURL = requestURL,
+    accessURL = accessURL,
+    authURL = authURL
+  )
+
+oauth.file$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+
+if (!file.exists(".httr-oauth")) {
+  save(oauth.file, file = ".httr-oauth")
+}
 
 # file.remove("tweets.json")
 # filterStream("tweets2.json", timeout = 60, track = "DietCoke",  oauth = my_oauth)
@@ -18,20 +31,23 @@ save(my_oauth, file = ".httr-oauth")
 # tweets.df <- parseTweets("tweets.json", verbose = FALSE)
 # tweets.df <- tweets.df[grep("@coke", tweets.df$text, ignore.case = TRUE), ]
 
-x <- data.frame(Company = c("Xero", "Xero", "Xero", "MYOB", "MYOB", "MYOB"),
-                Sentiment = c("Positive", "Negative", "Positive", "Positive", "Neutral", "Neutral"))
+# x <- data.frame(Company = c("Xero", "Xero", "Xero", "MYOB", "MYOB", "MYOB"),
+#                 Sentiment = c("Positive", "Negative", "Positive", "Positive", "Neutral", "Neutral"))
+# 
+# x %>% 
+#   group_by(Company, Sentiment) %>%
+#   tally %>%
+#   group_by(Company) %>%
+#   mutate(Perc = n/sum(n)) %>%
+#   plot_ly(x = ~Company, y = ~Perc, color = ~Sentiment, type = "bar") %>%
+#   layout(barmode = "stack")
 
-x %>% 
-  group_by(Company, Sentiment) %>%
-  tally %>%
-  group_by(Company) %>%
-  mutate(Perc = n/sum(n)) %>%
-  plot_ly(x = ~Company, y = ~Perc, color = ~Sentiment, type = "bar") %>%
-  layout(barmode = "stack")
-
-
-  
-
-
-
-  plot_ly(x = ~Company)
+tweets.df           <- parseTweets(ReadLastLines("tweets.json", n = 100), verbose = F)
+tweets.df           <- tweets.df[, c("country_code", "screen_name", "followers_count", "text", "created_at")]
+tweets.df$score     <- classify.aggregate(tweets.df$text, positive.words, negative.words)$score
+tweets.df$sentiment <- ifelse(tweets.df$score > 0, "Positive", ifelse(tweets.df$score < 0, "Negative", "Neutral"))
+tweets.df$company   <- NA
+tweets.df$company[grep("@xero", tweets.df$text, ignore.case = T)]       <- "Xero"
+tweets.df$company[grep("@myob", tweets.df$text, ignore.case = T)]       <- "MYOB"
+tweets.df$company[grep("@quickbooks", tweets.df$text, ignore.case = T)] <- "QuickBooks"
+tweets.df$company[grep("@sage", tweets.df$text, ignore.case = T)]       <- "Sage"
--- social-cocoon/ui.R
+++ social-cocoon/ui.R
@@ -1,17 +1,12 @@
-
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(plotly)
 
-shinyUI(fluidPage(
-  title = "Xero Mentions on Twitter Sentiment Analysis",
-  titlePanel("Xero Mentions on Twitter Sentiment Analysis"),
-  plotlyOutput('tweetPlot'),
-  hr(),
-  dataTableOutput('tweetTable')
-))
+shinyUI(
+  fluidPage(
+    title = "Accounting Software Sentiment Analysis",
+    # titlePanel("Xero Mentions on Twitter Sentiment Analysis"),
+    plotlyOutput("tweetPlot"),
+    hr(),
+    dataTableOutput("tweetTable")
+  )
+)
