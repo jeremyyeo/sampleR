c37dc6fc29a8d0be48945d0c6ee6e41bcfb08d5e
Jeremy Yeo <jeremy.yeo@xero.com>
2016-12-01 11:37:36 +1300

    archiving data as .rds vs parsing .json always

--- social-cocoon/listen.R
+++ social-cocoon/listen.R
@@ -3,6 +3,10 @@ require(streamR)
 
 load(".httr-oauth")  # load oauth file.
 
+# load lexicon for sentiment
+positive.words <- tail(readLines("positive-words.txt"), -35)
+negative.words <- tail(readLines("negative-words.txt"), -35)
+
 repeat {
   # listen to tweets and write to .json file.
   # get rid of file when it becomes too large.
@@ -16,4 +20,53 @@ repeat {
     oauth     = oauth.file
   )
   
+  if(file.exists("tweets.rds")) {
+    old.tweets.df <- readRDS("tweets.rds")
+  }
+  new.tweets.df <- parseTweets("tweets.json", verbose = F)
+  new.tweets.df$text       <- paste0(
+    '<a href="https://twitter.com/',
+    new.tweets.df$screen_name,
+    '/status/',
+    new.tweets.df$id_str,
+    '" target="_blank">',
+    new.tweets.df$text,
+    '</a>'
+  )
+  new.tweets.df            <- new.tweets.df[, c("created_at",
+                                                "country_code",
+                                                "screen_name",
+                                                "followers_count",
+                                                "text")]
+  new.tweets.df$screen_name <- paste0(
+    '<a href="https://twitter.com/',
+    new.tweets.df$screen_name,
+    '" target="_blank">',
+    new.tweets.df$screen_name,
+    '</a>'
+  )
+  new.tweets.df$created_at <- strptime(new.tweets.df$created_at, "%a %b %d %H:%M:%S +0000 %Y", tz = "UTC")
+  new.tweets.df$score      <- classify.aggregate(new.tweets.df$text, positive.words, negative.words)$score
+  new.tweets.df$sentiment  <- as.factor(ifelse(
+    new.tweets.df$score > 0,
+    "Positive",
+    ifelse(new.tweets.df$score < 0, "Negative", "Neutral")
+  ))
+  new.tweets.df$company    <- NA
+  new.tweets.df$company[grep("xero", new.tweets.df$text, ignore.case = T)]       <- "Xero"
+  new.tweets.df$company[grep("myob", new.tweets.df$text, ignore.case = T)]       <- "MYOB"
+  new.tweets.df$company[grep("quickbooks", new.tweets.df$text, ignore.case = T)] <- "QuickBooks"
+  new.tweets.df$company[grep("sage", new.tweets.df$text, ignore.case = T)]       <- "Sage"
+  new.tweets.df$company <- factor(new.tweets.df$company,
+                                  levels = c("Xero", "MYOB", "QuickBooks", "Sage")
+  )
+  
+  if(exists("old.tweets.df")) {
+    old.tweets.df <- rbind.data.frame(old.tweets.df, new.tweets.df)
+  } else {
+    old.tweets.df <- new.tweets.df
+  }
+  
+  saveRDS(old.tweets.df, "tweets.rds")
+
 }
--- social-cocoon/server.R
+++ social-cocoon/server.R
@@ -39,31 +39,30 @@ negative.words <- tail(readLines("negative-words.txt"), -35)
 
 # system("Rscript listen.R", wait = F)
 shinyServer(function(input, output, session) {
-  tweets.df <- reactiveFileReader(1000, session, filePath = "tweets.json", readFunc = parseTweets)
-
+  # new.tweets.df <- reactiveFileReader(1000, session, filePath = "tweets.json", readFunc = parseTweets)
+  tweets.df <- reactiveFileReader(1000, session, filePath = "tweets.rds", readFunc = readRDS)
+  
   data <- reactive({
-    # invalidateLater(10000)
-    tweets.df           <- parseTweets("tweets.json", verbose = F)
-    tweets.df           <- unlist(lapply(readTweets("tweets.json"), '[[', 'created_at'))
+    # tweets.df           <- parseTweets("tweets.json", verbose = F)
     # tweets.df           <- tweets.df()
-    tweets.df$text      <- paste0('<a href="https://twitter.com/', 
-                                  tweets.df$screen_name, 
-                                  '/status/',
-                                  tweets.df$id_str,
-                                  '" target="_blank">',
-                                  tweets.df$text,
-                                  '</a>')
-    tweets.df           <- tweets.df[, c("created_at", "country_code", "screen_name", "followers_count", "text")]
-    tweets.df$created_at <- strptime(tweets.df$created_at, "%a %b %d %H:%M:%S +0000 %Y", tz = "UTC")
-    tweets.df$score     <- classify.aggregate(tweets.df$text, positive.words, negative.words)$score
-    tweets.df$sentiment <- as.factor(ifelse(tweets.df$score > 0, "Positive", ifelse(tweets.df$score < 0, "Negative", "Neutral")))
-    tweets.df$company   <- NA
-    tweets.df$company[grep("@xero", tweets.df$text, ignore.case = T)]       <- "Xero"
-    tweets.df$company[grep("@myob", tweets.df$text, ignore.case = T)]       <- "MYOB"
-    tweets.df$company[grep("@quickbooks", tweets.df$text, ignore.case = T)] <- "QuickBooks"
-    tweets.df$company[grep("@sage", tweets.df$text, ignore.case = T)]       <- "Sage"
-    tweets.df$company <- factor(tweets.df$company, levels = c("Xero", "MYOB", "QuickBooks", "Sage"))
-    tweets.df
+    # tweets.df$text      <- paste0('<a href="https://twitter.com/', 
+    #                               tweets.df$screen_name, 
+    #                               '/status/',
+    #                               tweets.df$id_str,
+    #                               '" target="_blank">',
+    #                               tweets.df$text,
+    #                               '</a>')
+    # tweets.df            <- tweets.df[, c("created_at", "country_code", "screen_name", "followers_count", "text")]
+    # tweets.df$created_at <- strptime(tweets.df$created_at, "%a %b %d %H:%M:%S +0000 %Y", tz = "UTC")
+    # tweets.df$score     <- classify.aggregate(tweets.df$text, positive.words, negative.words)$score
+    # tweets.df$sentiment <- as.factor(ifelse(tweets.df$score > 0, "Positive", ifelse(tweets.df$score < 0, "Negative", "Neutral")))
+    # tweets.df$company   <- NA
+    # tweets.df$company[grep("@xero", tweets.df$text, ignore.case = T)]       <- "Xero"
+    # tweets.df$company[grep("@myob", tweets.df$text, ignore.case = T)]       <- "MYOB"
+    # tweets.df$company[grep("@quickbooks", tweets.df$text, ignore.case = T)] <- "QuickBooks"
+    # tweets.df$company[grep("@sage", tweets.df$text, ignore.case = T)]       <- "Sage"
+    # tweets.df$company <- factor(tweets.df$company, levels = c("Xero", "MYOB", "QuickBooks", "Sage"))
+    tweets.df()
   })
   
   output$tweetTable <- DT::renderDataTable({
