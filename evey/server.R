aae30b102b5084285cb5beea475575208c618044
Jeremy <jeremyyeo@gmail.com>
2017-03-12 15:39:44 +1300

    ignored .credentials and changes to evey

--- evey/server.R
+++ evey/server.R
@@ -2,8 +2,11 @@
 
 library(shiny)
 library(lubridate)
+library(plotly)
 
 server <- function(input, output, session) {
+  amberPalette <- c("#FFF8E1", "#FFECB3", "#FFE082", "#FFD54F", "#FFCA28", 
+                    "#FFC107", "#FFB300", "#FFA000", "#FF8F00", "#FF6F00")
   
   d.left <- reactive({
     invalidateLater(1000*60*60*24, session)
@@ -34,13 +37,75 @@ server <- function(input, output, session) {
     ) * 24 * 60 * 60
   })
   
-  output$printleft <- renderPrint({
-    data.frame(days    = round(d.left(), 0),
-               hours   = round(h.left(), 0),
-               minutes = round(m.left(), 0),
-               seconds = s.left())
+  # output$printleft <- renderPrint({
+  #   data.frame(days    = round(d.left(), 0),
+  #              hours   = round(h.left(), 0),
+  #              minutes = round(m.left(), 0),
+  #              seconds = s.left())
+  # })
+  
+  totaldays <- as.numeric(
+    ymd_hms("2017-04-26 11:30:00", tz = "Australia/Sydney") - 
+      ymd_hms("2017-02-26 06:40:00", tz = "Pacific/Auckland") 
+  )
+  
+  
+  output$plotdleft <- renderPlotly({
+    plot_ly(x = ~d.left(), y = "1", type = "bar", orientation = "h", hoverinfo = "none",
+            marker = list(color = amberPalette[6], line = list(color = "black", width = 1))) %>%
+      layout(xaxis = list(fixedrange = T, title = "", range = c(0, totaldays)),
+             yaxis = list(fixedrange = T, showticklabels = F),
+             annotations = list(x = ~d.left()/2, 
+                                y = "1", 
+                                text = ~trunc(d.left()), 
+                                xanchor = "center",
+                                showarrow = F,
+                                font = list(size = 15))) %>%
+      config(displayModeBar = F, staticPlot = T)
+  }) 
+  
+  output$plothleft <- renderPlotly({
+    plot_ly(x = ~h.left(), y = "1", type = "bar", orientation = "h", hoverinfo = "none",
+            marker = list(color = amberPalette[7], line = list(color = "black", width = 1))) %>%
+      layout(xaxis = list(fixedrange = T, title = "", range = c(0, totaldays * 24)),
+             yaxis = list(fixedrange = T, showticklabels = F),
+             annotations = list(x = ~h.left()/2, 
+                                y = "1", 
+                                text = ~trunc(h.left()), 
+                                xanchor = "center",
+                                showarrow = F,
+                                font = list(size = 15))) %>%
+      config(displayModeBar = F, staticPlot = T)
+  }) 
+  
+  output$plotmleft <- renderPlotly({
+    plot_ly(x = ~m.left(), y = "1", type = "bar", orientation = "h", hoverinfo = "none",
+            marker = list(color = amberPalette[8], line = list(color = "black", width = 1))) %>%
+      layout(xaxis = list(fixedrange = T, title = "", range = c(0, totaldays * 24 * 60)),
+             yaxis = list(fixedrange = T, showticklabels = F),
+             annotations = list(x = ~m.left()/2, 
+                                y = "1", 
+                                text = ~trunc(m.left()), 
+                                xanchor = "center",
+                                showarrow = F,
+                                font = list(size = 15))) %>%
+      config(displayModeBar = F, staticPlot = T)
   })
   
+  output$plotsleft <- renderPlotly({
+    plot_ly(x = ~s.left(), y = "1", type = "bar", orientation = "h", hoverinfo = "none",
+            marker = list(color = amberPalette[9], line = list(color = "black", width = 1))) %>%
+      layout(xaxis = list(fixedrange = T, title = "", range = c(0, totaldays * 24 * 60 * 60)),
+             yaxis = list(fixedrange = T, showticklabels = F),
+             annotations = list(x = ~s.left()/2, 
+                                y = "1", 
+                                text = ~trunc(s.left()), 
+                                xanchor = "center",
+                                showarrow = F,
+                                font = list(size = 15))) %>%
+      config(displayModeBar = F, staticPlot = T)
+  }) 
+
   output$nzTime <- renderInfoBox({
     invalidateLater(1000, session)
     dt <- format(with_tz(Sys.time(), tzone = "Pacific/Auckland"), format = "%H:%M:%S %a, %B %d, %Y")
--- evey/ui.R
+++ evey/ui.R
@@ -2,20 +2,46 @@
 
 library(shiny)
 library(shinydashboard)
+library(plotly)
 
 ui <- dashboardPage(
   skin = "red",
-  dashboardHeader(),
+  dashboardHeader(title = icon("heart")),
   dashboardSidebar(
     disable = TRUE
     ),
   dashboardBody(
     fluidRow(
-        infoBoxOutput("nzTime", width = 6),
-        infoBoxOutput("sgTime", width = 6)
+        infoBoxOutput("sgTime", width = 6),
+        infoBoxOutput("nzTime", width = 6)
     ),
     fluidRow(
-      verbatimTextOutput("printleft")
+      box(width = 12, solidHeader = T, h5("Countdown to Sydney", align = "center"))
+    ),
+    fluidRow(
+      box(title = "Days Left", status = "danger",
+          width = 3, solidHeader = T, collapsible = F,
+          style = "padding:0px;",
+          plotlyOutput("plotdleft", height = "150px")
+      ),
+      box(title = "Hours Left", status = "danger",
+          width = 3, solidHeader = T, collapsible = F,
+          style = "padding:0px;",
+          plotlyOutput("plothleft", height = "150px")
+      ),
+      box(title = "Minutes Left", status = "danger",
+          width = 3, solidHeader = T, collapsible = F,
+          style = "padding:0px;",
+          plotlyOutput("plotmleft", height = "150px")
+      ),
+      box(title = "Seconds Left", status = "danger",
+          width = 3, solidHeader = T, collapsible = F,
+          style = "padding:0px;",
+          plotlyOutput("plotsleft", height = "150px")
+      ) 
+    ),
+    fluidRow(
+      box(width = 12, solidHeader = T, h5("Weather Forecast", align = "center"))
     )
   )
 )
