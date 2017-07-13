2ea309174ce5882f4b4efff2dd483c1ea4b84ea0
Jeremy Yeo <jeremy.yeo@xero.com>
2017-07-13 15:55:40 +1200

    added error for non existent webhook token

--- useful-functions/messageslack.R
+++ useful-functions/messageslack.R
@@ -1,8 +1,12 @@
 messageslack <- function(message, verbose = F) {
   # Sends a character string to specific slack channel.
-  # Requires a slack webhook token
+  # Requires a slack webhook token: https://api.slack.com/incoming-webhooks
   if (!is.character(message)) {
     stop("message argument is not of type: character.")
+  }
+
+  if (!file.exists(".slackwebhooktoken")) {
+    stop("webhook token does not exist.")
   }
 
   request <-
