95366b3d5dc75f88ccaaa4c1c879b1d1a5dd8d43
Jeremy Yeo <jeremy.yeo@xero.com>
2017-07-14 13:47:49 +1200

    added chartonum function

---
+++ useful-functions/chartonum.R
@@ -0,0 +1,62 @@
+# converts text representation of numbers into actual numeric format.
+# e.g. "one hundred" to 100, "five thousand and five" to 5005
+# source: https://github.com/ghewgill/text2num
+
+small <- 
+  c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    30, 40, 50, 60, 70, 80, 90)
+names(small) <-
+  c("zero", "one", "two", "three", "four", "five",
+    "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+    "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+    "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety")
+magnitude <-
+  c(100,
+    1000,
+    1000000,
+    1000000000,
+    1000000000000,
+    1000000000000000,
+    1000000000000000000,
+    1000000000000000000000,
+    1000000000000000000000000,
+    1000000000000000000000000000,
+    1000000000000000000000000000000,
+    1000000000000000000000000000000000)
+names(magnitude) <-
+  c("hundred",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+    "sextillion",
+    "septillion",
+    "octillion",
+    "nonillion",
+    "decillion")
+
+chartonum <- function(x) {
+  a <- unlist(strsplit(x, " "))
+  a <- a[a != "and"]
+  n <- 0
+  g <- 0
+  for (i in a) {
+    x <- as.numeric(small[match(i, names(small))])
+    if (!is.na(x)) {
+      g <- g + x
+    } else {
+      x <- as.numeric(magnitude[match(i, names(magnitude))])
+      if (!is.na(x)) {
+        n <- n + (g * x)
+        g <- 0
+      } else {
+        stop("Unknown number: ", i)
+      }
+    }
+  }
+  return(n + g)
+}
